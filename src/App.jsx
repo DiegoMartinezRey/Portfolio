d5bae562f43ea7db62deca71fb2ee58d8de286e7
Diego Martinez <diegomartinez@MacBook-Air-de-Diego.local>
2024-02-14 11:07:07 +0100

    Add routes and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
       <div>Hola mundo</div>
     </>
   );
