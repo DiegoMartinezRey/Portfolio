526894b44024d29ea02b169f2c32db3267733ca1
Diego Martinez <diegomrey1@gmail.com>
2024-03-07 11:39:16 +0100

    Add icons from fontawsome

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,3 +1,6 @@
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { faFile } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import "./Header.css";
 
@@ -6,9 +9,11 @@ const Header = () => {
     <>
       <div className="flexHeader">
         <div className="profileInfo">
-          <div>
-            <h3>Hello! my name is</h3>
-            <h1 className="name_style">Diego Martinez</h1>
+          <div className="infoFlex">
+            <div>
+              <h3>Hello! my name is</h3>
+              <h1 className="name_style">Diego Martinez</h1>
+            </div>
             <h3 className="description_style">
               Professional in mechatronics engineering with a master's degree in
               full stack development.
@@ -16,7 +21,11 @@ const Header = () => {
             <div className="profileImage"></div>
           </div>
         </div>
-        <div>Social media</div>
+        <div className="iconFlex">
+          <FontAwesomeIcon icon={faGithub} size="2xl" className="iconStyle" />
+          <FontAwesomeIcon icon={faLinkedin} size="2xl" className="iconStyle" />
+          <FontAwesomeIcon icon={faFile} size="2xl" className="iconStyle" />
+        </div>
       </div>
     </>
   );
