16c5bc4592cf29b75998fa0253e16a76719a5e81
Diego Martinez <diegomrey1@gmail.com>
2024-03-07 13:02:13 +0100

    Change description of the projects and add some styles

--- src/containers/AboutMe/AboutMe.jsx
+++ src/containers/AboutMe/AboutMe.jsx
@@ -7,16 +7,16 @@ const AboutMe = () => {
       <div className="flex_AboutMe">
         <h2>About me</h2>
         <p>
-          Profesional en ingeniería mecatrónica con experiencia en el área de
-          servicio técnico, automatización, programación, soporte comercial y
-          formulación de soluciones en proyectos, con conocimiento en áreas de
-          desarrollo tecnológico y de gestión. Soy una persona apasionada por la
-          tecnología y la innovación en donde he aprendido a adaptarme a los
-          cambios y a las diferentes dinámicas que se presentan en el campo
-          digital desarrollando actividades en un ambiente de trabajo en equipo.
-          Busco ampliar mis conocimientos a partir de nuevas experiencias en las
-          diferentes áreas productivas aportando mis conocimientos para seguir
-          creciendo tanto en mi vida personal como profesional.
+          Professional in mechatronics engineering with experience in the area
+          of technical service, automation, programming,commercial support and
+          formulation of project solutions, with knowledge in areas of
+          technological development andmanagement. I am a person passionate
+          about technology and application development where I have learned to
+          adaptto changes and the different dynamics that arise in the digital
+          field developing activities in a teamwork environment. Iseek to expand
+          my knowledge as a full stack developer from new experiences in
+          different productive areas contributingmy knowledge to continue
+          growing both in my personal and professional life.
         </p>
       </div>
     </>
